62deaf5eb160fd458fe927c8ff363dbbad29ef75
Ranjith Varma <ranjith.varma@hatio.in>
2020-07-30 18:54:51 +0530

    initial commit

---
+++ packages/web/custom.d.ts
@@ -0,0 +1,11 @@
+declare module "*.svg" {
+  // eslint-disable-next-line no-unused-vars
+  import React = require("react");
+  export const ReactComponent: React.SFC<React.SVGProps<SVGSVGElement>>;
+  const src: string;
+  export default src;
+}
+
+declare module "*.scss";
+
+declare module "common/src/main";
